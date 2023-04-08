f9263969a4b2add95d14ffeeea463791075c5ef4
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2023-04-08 17:35:35 +0300

    added one more preloader

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 //import { MainView } from "./MainView"
 
 const imgSrc = 'https://static.boredpanda.com/blog/wp-content/uploads/2019/05/marvel-avengers-movie-actors-90s-houseofmat-3-5cd15080e4ff4__700.jpg';
-export function TestCreate() {
+export function About() {
 
 
     return ( // nope, guess i wont use that for now...
@@ -28,7 +28,7 @@ export function TestCreate() {
                                 <h1>Movies catalog made with ReactJS</h1>
 
                                 <img src={imgSrc} alt="Cast" />
-                                <p>
+                                <p style={{fontSize: '1.8rem', letterSpacing:'0.4px'}}>
                                     Avengers: Infinity War has finally hit theaters, and fans of the Marvel Cinematic Universe are naturally asking the most important question of them all: what if The Avengers was made in the 1990s? It's a question as old as time, or at least as old as the '90s.
 
                                     It's always fun to hypothetically cast the most popular actors of a decade, and attempt to fit their personality and on-screen presence to a specific character. Who might have played Iron Man (considering Robert Downey Jr. was still an avid socialite)? What would happen if the MCU started 20 years ago? There are dozens of actors and actresses who almost made it into Marvel movies, and several of them could have led their own franchises at the height of their '90s popularity.
--- src/components/MovieSlider.js
+++ src/components/MovieSlider.js
@@ -1,23 +1,30 @@
 import { useState, useEffect } from "react";
 import { url, imgSrc } from "../secretStuff";
 import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css'; 
-/*--- READ ---- HERE -----*/ 
+import 'react-multi-carousel/lib/styles.css';
+import { Preloader } from "./Preloader";
+
+/*--- READ ---- HERE -----*/
 /* In order to use this component you will need an access key from 
 https://www.themoviedb.org/
 
 So, if you dont know how to get one, watch this video: https://www.youtube.com/watch?v=FlFyrOEz2S4 
 and replace the api key in the url with your own. 
 
-You can checkout the API documentation here: https://developers.themoviedb.org/3/getting-started/introduction*/ 
+You can checkout the API documentation here: https://developers.themoviedb.org/3/getting-started/introduction*/
 
 export function MovieSlider() {
     const [moveis, setMovies] = useState([]);
+    const [isLoading, setIsloading] = useState(false);
 
     useEffect(() => {
+        setIsloading(true)
         fetch(url)
             .then(response => response.json())
-            .then(m => setMovies(m.results))
+            .then(m => {
+                setMovies(m.results);
+                setIsloading(false);
+            })
             .catch(err => setMovies([]))
     }, []);
 
@@ -41,37 +48,41 @@ export function MovieSlider() {
     };
 
     return (
-            <div class="slider movie-items">
-                <div class="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="hero-ct">
-                                <p style={{color: 'white'}}>
-                                <h2>Trending now</h2>
-                                </p>
+        <>
+            {isLoading ? <Preloader /> :
+                <div className="slider movie-items">
+                    < div className="container" >
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="hero-ct">
+                                    <p style={{ color: 'white' }}>
+                                        <h2>Trending now</h2>
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="slick-multiItemSlider">
-                            <Carousel responsive={responsive}
-                                autoPlay={true}
-                                autoPlaySpeed={2000}>
-                                {moveis.map((m) => (
-                                    <div class="movie-item" key={m.id}>
-                                        <div class="mv-img">
-                                            <img src={imgSrc + m.poster_path} alt="none" width="285" height="437" />
+                        <div className="row">
+                            <div className="slick-multiItemSlider">
+                                <Carousel responsive={responsive}
+                                    autoPlay={true}
+                                    autoPlaySpeed={2000}>
+                                    {moveis.map((m) => (
+                                        <div className="movie-item" key={m.id}>
+                                            <div className="mv-img">
+                                                <img src={imgSrc + m.poster_path} alt="none" width="285" height="437" />
+                                            </div>
+                                            <div className="title-in">
+                                                <h6><a href={`https://www.themoviedb.org/movie/${m.id}`}>{m.title}</a></h6>
+                                                <p><i className="ion-android-star"></i><span>{Math.floor(m.vote_average)}</span> /10</p>
+                                            </div>
                                         </div>
-                                        <div class="title-in">
-                                            <h6><a href={`https://www.themoviedb.org/movie/${m.id}`}>{m.title}</a></h6>
-                                            <p><i class="ion-android-star"></i><span>{Math.floor(m.vote_average)}</span> /10</p>
-                                        </div>
-                                    </div>
-                                ))}
-                            </Carousel>
+                                    ))}
+                                </Carousel>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div>
+                    </div >
+                </div >
+            }
+        </>
     );
 }
